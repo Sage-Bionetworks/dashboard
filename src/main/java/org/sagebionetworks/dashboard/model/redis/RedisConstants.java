8846abd075be2343a4f0d9c159c8cd5cba89227d
eric-wu <eric.wu@sagebase.org>
2013-11-07 16:54:55 -0800

    Up the key repire days from 90 to 180 days.

--- src/main/java/org/sagebionetworks/dashboard/model/redis/RedisConstants.java
+++ src/main/java/org/sagebionetworks/dashboard/model/redis/RedisConstants.java
@@ -2,6 +2,6 @@ package org.sagebionetworks.dashboard.model.redis;
 
 public class RedisConstants {
 
-    /** By default, we cache the metrics for 90 days. */
-    public static final long EXPIRE_DAYS = 90L;
+    /** By default, we cache the metrics for 180 days. */
+    public static final long EXPIRE_DAYS = 180L;
 }
