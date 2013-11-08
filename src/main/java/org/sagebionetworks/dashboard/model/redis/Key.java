19fd3de847a514c545b626a9e24fc0c82a887229
eric-wu <eric.wu@sagebase.org>
2013-11-08 15:09:23 -0800

    Fix the name-id key names.

--- src/main/java/org/sagebionetworks/dashboard/model/redis/Key.java
+++ src/main/java/org/sagebionetworks/dashboard/model/redis/Key.java
@@ -8,7 +8,7 @@ public class Key {
     public static final String SEPARATOR = ":";
 
     /** Name --> ID mappings */
-    public static final String NAME_ID = NameSpace.name + SEPARATOR + NameSpace.name;
+    public static final String NAME_ID = NameSpace.name + SEPARATOR + NameSpace.id;
     /** ID --> name mappings */
-    public static final String ID_NAME = NameSpace.id + SEPARATOR + NameSpace.id;
+    public static final String ID_NAME = NameSpace.id + SEPARATOR + NameSpace.name;
 }
