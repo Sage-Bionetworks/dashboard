b8f84794a3d94f2d69f5ea4098b3b9376a6404b8
eric-wu <eric.wu@sagebase.org>
2013-11-19 23:50:26 -0800

    Add LockDao for distributed locking.

--- src/main/java/org/sagebionetworks/dashboard/dao/redis/RedisConstants.java
+++ src/main/java/org/sagebionetworks/dashboard/dao/redis/RedisConstants.java
@@ -4,4 +4,7 @@ class RedisConstants {
 
     /** By default, we cache the metrics for 180 days. */
     static final long EXPIRE_DAYS = 180L;
+
+    /** Lock on file processing is set as 30 minutes. */
+    static final long LOCK_EXPIRE_MINUTES = 30L;
 }
