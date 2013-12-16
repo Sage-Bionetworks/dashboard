46ab9e515684a969c9671756413b668b773f011c
eric-wu <eric.wu@sagebase.org>
2013-12-16 01:39:04 -0800

    Add id -> name converters.

---
+++ src/main/java/org/sagebionetworks/dashboard/service/UserIdToName.java
@@ -0,0 +1,25 @@
+package org.sagebionetworks.dashboard.service;
+
+import javax.annotation.Resource;
+
+import org.sagebionetworks.dashboard.dao.SynapseDao;
+import org.sagebionetworks.dashboard.model.CountDataPoint;
+import org.springframework.stereotype.Service;
+
+@Service("userIdToName")
+public class UserIdToName implements CountDataPointConverter {
+
+    @Resource
+    private SynapseDao synapseDao;
+
+    @Override
+    public CountDataPoint convert(final CountDataPoint in) {
+        String id = in.getId();
+        String name = synapseDao.getUserDisplayName(id);
+        if (name == null) {
+            name = id;
+        }
+        CountDataPoint out = new CountDataPoint(name, in.getCount());
+        return out;
+    }
+}
---
+++ src/test/java/org/sagebionetworks/dashboard/service/EntityIdToNameTest.java
@@ -0,0 +1,34 @@
+package org.sagebionetworks.dashboard.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.sagebionetworks.dashboard.dao.SynapseDao;
+import org.sagebionetworks.dashboard.model.CountDataPoint;
+import org.springframework.test.util.ReflectionTestUtils;
+
+public class EntityIdToNameTest {
+
+    @Test
+    public void test() {
+
+        // Mock
+        SynapseDao mockSynapse = mock(SynapseDao.class);
+        when(mockSynapse.getEntityName("id")).thenReturn("name");
+        when(mockSynapse.getEntityName("null")).thenReturn(null);
+        EntityIdToName idToName = new EntityIdToName();
+        ReflectionTestUtils.setField(idToName, "synapseDao", mockSynapse, SynapseDao.class);
+
+        // Test
+        CountDataPoint in = new CountDataPoint("id", 5L);
+        CountDataPoint out = idToName.convert(in);
+        assertEquals("name", out.getId());
+        assertEquals(5L, out.getCount());
+        in = new CountDataPoint("null", 3L);
+        out = idToName.convert(in);
+        assertEquals("null", out.getId());
+        assertEquals(3L, out.getCount());
+    }
+}
