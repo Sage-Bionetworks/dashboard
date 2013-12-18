d4d009968d773df1d7463366d14df6f4206f672b
eric-wu <eric.wu@sagebase.org>
2013-12-18 14:26:14 -0800

    Simplify the query service. Post processing of the results will be moved to the UI package.

--- src/main/java/org/sagebionetworks/dashboard/service/MetricQueryService.java
+++ src/main/java/org/sagebionetworks/dashboard/service/MetricQueryService.java
@@ -1,12 +1,10 @@
 package org.sagebionetworks.dashboard.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
 
 import org.joda.time.DateTime;
-import org.sagebionetworks.dashboard.dao.NameIdDao;
 import org.sagebionetworks.dashboard.dao.TimeSeriesDao;
 import org.sagebionetworks.dashboard.dao.UniqueCountDao;
 import org.sagebionetworks.dashboard.model.Aggregation;
@@ -24,37 +22,16 @@ public class MetricQueryService {
     @Resource
     private UniqueCountDao uniqueCountDao;
 
-    @Resource
-    private NameIdDao nameIdDao;
-
-    public List<TimeDataPoint> getTimeSeries(
-            String metricId, DateTime from, DateTime to, Statistic s, Aggregation a) {
+    public List<TimeDataPoint> getTimeSeries(String metricId, DateTime from, DateTime to,
+            Statistic s, Aggregation a) {
         return timeSeriesDao.timeSeries(metricId, from, to, s, a);
     }
 
-    public List<CountDataPoint> getTop25(String metricId, DateTime timestamp) {
-        return uniqueCountDao.topCounts(metricId, timestamp, 25);
-    }
-
-    public List<CountDataPoint> getTop25(String metricId, DateTime timestamp,
-            CountDataPointConverter converter) {
-        List<CountDataPoint> results = getTop25(metricId, timestamp);
-        for (int i = 0; i < results.size(); i++) {
-            results.set(i, converter.convert(results.get(i)));
-        }
-        return results;
+    public List<CountDataPoint> getTop(String metricId, DateTime timestamp, int n) {
+        return uniqueCountDao.topCounts(metricId, timestamp, n);
     }
 
     public List<TimeDataPoint> getUniqueCount(String metricId, DateTime from, DateTime to) {
         return uniqueCountDao.uniqueCounts(metricId, from, to);
-    }
-
-    public List<List<TimeDataPoint>> getUniqueCount(List<String> metricIds,
-            DateTime from, DateTime to) {
-        List<List<TimeDataPoint>> results = new ArrayList<>(metricIds.size());
-        for (String metricId : metricIds) {
-            results.add(uniqueCountDao.uniqueCounts(metricId, from, to));
-        }
-        return results;
     }
 }
