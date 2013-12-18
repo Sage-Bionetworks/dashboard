56f8775b6b19371f9b8cac5d5ff1a1e83d5c95bc
eric-wu <eric.wu@sagebase.org>
2013-12-17 19:00:21 -0800

    Remove MetricToRead. (Reading metrics will be done in the UI package.)

--- src/main/java/org/sagebionetworks/dashboard/App.java
+++ src/main/java/org/sagebionetworks/dashboard/App.java
@@ -7,15 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import org.joda.time.DateTime;
-import org.sagebionetworks.dashboard.model.Aggregation;
-import org.sagebionetworks.dashboard.model.CountDataPoint;
-import org.sagebionetworks.dashboard.model.MetricType;
-import org.sagebionetworks.dashboard.model.Statistic;
-import org.sagebionetworks.dashboard.model.TimeDataPoint;
-import org.sagebionetworks.dashboard.service.MetricQueryService;
-import org.sagebionetworks.dashboard.service.MetricRegistry;
-import org.sagebionetworks.dashboard.service.MetricToRead;
 import org.sagebionetworks.dashboard.service.UpdateCallback;
 import org.sagebionetworks.dashboard.service.UpdateService;
 import org.springframework.context.ApplicationContext;
@@ -64,45 +55,6 @@ public class App {
             } finally {
                 if (is != null) {
                     is.close();
-                }
-            }
-        }
-
-        // Query
-        MetricQueryService queryService = context.getBean(MetricQueryService.class);
-        MetricRegistry registry = context.getBean(MetricRegistry.class);
-        List<MetricToRead> metrics = registry.metricsToRead();
-        for (MetricToRead metric : metrics) {
-            System.out.println(metric.getName());
-            System.out.println(metric.getId());
-            if (MetricType.TOP == metric.getType()) {
-                List<CountDataPoint> topList = queryService.getTop25(
-                        metric.getId(), new DateTime(2013, 10, 21, 12, 22));
-                for (CountDataPoint d : topList) {
-                    System.out.println(d);
-                }
-                topList = queryService.getTop25(
-                        metric.getId(), new DateTime(2013, 10, 23, 12, 22));
-                for (CountDataPoint d : topList) {
-                    System.out.println(d);
-                }
-            }
-            if (MetricType.UNIQUE_COUNT == metric.getType()) {
-                List<TimeDataPoint> uniqueList = queryService.getUniqueCount(metric.getId(),
-                        new DateTime(2013, 10, 21, 12, 22),
-                        new DateTime(2013, 10, 27, 12, 22));
-                for (TimeDataPoint d : uniqueList) {
-                    System.out.println(d);
-                }
-            }
-            if (MetricType.TIME_SERIES == metric.getType()) {
-                List<TimeDataPoint> tsList = queryService.getTimeSeries(metric.getId(),
-                        new DateTime(2013, 10, 21, 12, 22),
-                        new DateTime(2013, 10, 22, 12, 22),
-                        Statistic.avg,
-                        Aggregation.hour);
-                for (TimeDataPoint d : tsList) {
-                    System.out.println(d);
                 }
             }
         }
