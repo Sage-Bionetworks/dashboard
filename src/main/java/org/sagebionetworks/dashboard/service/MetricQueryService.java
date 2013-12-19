95bf6495604d8bd64c8672faf28df6f950620281
eric-wu <eric.wu@sagebase.org>
2013-12-19 05:53:44 -0800

    Change the metric query service to take a metric name instead of a metric ID.

--- src/main/java/org/sagebionetworks/dashboard/service/MetricQueryService.java
+++ src/main/java/org/sagebionetworks/dashboard/service/MetricQueryService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import org.joda.time.DateTime;
+import org.sagebionetworks.dashboard.dao.NameIdDao;
 import org.sagebionetworks.dashboard.dao.TimeSeriesDao;
 import org.sagebionetworks.dashboard.dao.UniqueCountDao;
 import org.sagebionetworks.dashboard.model.Aggregation;
@@ -17,21 +18,49 @@ import org.springframework.stereotype.Service;
 public class MetricQueryService {
 
     @Resource
+    private NameIdDao nameIdDao;
+
+    @Resource
     private TimeSeriesDao timeSeriesDao;
 
     @Resource
     private UniqueCountDao uniqueCountDao;
 
-    public List<TimeDataPoint> getTimeSeries(String metricId, DateTime from, DateTime to,
+    public List<TimeDataPoint> getTimeSeries(String metricName, DateTime from, DateTime to,
             Statistic s, Aggregation a) {
+
+        if (metricName == null || metricName.isEmpty()) {
+            throw new IllegalArgumentException("Metric name cannot be null or empty.");
+        }
+        String metricId = getMetricId(metricName);
         return timeSeriesDao.timeSeries(metricId, from, to, s, a);
     }
 
-    public List<CountDataPoint> getTop(String metricId, DateTime timestamp, int n) {
+    public List<CountDataPoint> getTop(String metricName, DateTime timestamp, int n) {
+
+        if (metricName == null || metricName.isEmpty()) {
+            throw new IllegalArgumentException("Metric name cannot be null or empty.");
+        }
+        String metricId = getMetricId(metricName);
         return uniqueCountDao.topCounts(metricId, timestamp, n);
     }
 
-    public List<TimeDataPoint> getUniqueCount(String metricId, DateTime from, DateTime to) {
+    public List<TimeDataPoint> getUniqueCount(String metricName, DateTime from, DateTime to) {
+
+        if (metricName == null || metricName.isEmpty()) {
+            throw new IllegalArgumentException("Metric name cannot be null or empty.");
+        }
+        String metricId = getMetricId(metricName);
         return uniqueCountDao.uniqueCounts(metricId, from, to);
+    }
+
+    private String getMetricId(String metricName) {
+        if(!metricName.endsWith("Metric")) {
+            metricName = metricName + "Metric";
+        }
+        if (!nameIdDao.hasName(metricName)) {
+            throw new IllegalArgumentException(metricName + " is not a valid metric name.");
+        }
+        return nameIdDao.getId(metricName);
     }
 }
