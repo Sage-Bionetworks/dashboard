fee0dd16756eeb11c2ca456527161edc985dabfc
eric-wu <eric.wu@sagebase.org>
2013-12-23 15:52:06 -0800

    Minor fixes per code review.

--- src/main/java/org/sagebionetworks/dashboard/parse/ProdFilter.java
+++ src/main/java/org/sagebionetworks/dashboard/parse/ProdFilter.java
@@ -1,5 +1,6 @@
 package org.sagebionetworks.dashboard.parse;
 
+// TODO: This does not distinguish prod from staging
 public class ProdFilter implements RecordFilter {
     @Override
     public boolean matches(Record record) {
