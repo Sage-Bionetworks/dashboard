a3f99e9f48d037a4d8fca953a94f1f64d8e7f72f
eric-wu <eric.wu@sagebase.org>
2014-03-19 01:03:31 -0700

    Clean imports.

--- src/main/java/org/sagebionetworks/dashboard/context/DashboardContext.java
+++ src/main/java/org/sagebionetworks/dashboard/context/DashboardContext.java
@@ -6,7 +6,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Properties;
 
 import org.slf4j.Logger;
