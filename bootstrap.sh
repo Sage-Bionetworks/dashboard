b44db6863e45732444def385a9d96feb3be70bc0
eric-wu <eric.wu@sagebase.org>
2014-06-19 12:34:17 -0700

    Noninteractive upgrade.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-echo 'deb http://mirrors.kernel.org/debian wheezy-backports main' >> /etc/apt/sources.list
-yes | apt-get update
+export DEBIAN_FRONTEND=noninteractive
 
-yes | apt-get -t wheezy-backports install redis-server
-yes | apt-get -t wheezy-backports install openjdk-7-jdk
+apt-get autoclean
+apt-get autoremove
+
+echo 'deb http://mirrors.kernel.org/debian wheezy-backports main' >> /etc/apt/sources.list
+apt-get --quiet --yes update
+apt-get --quiet --yes --target-release wheezy-backports upgrade
+
+apt-get --quiet --yes --target-release wheezy-backports install redis-server
+apt-get --quiet --yes --target-release wheezy-backports install openjdk-7-jdk
 
