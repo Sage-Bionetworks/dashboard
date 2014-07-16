b3ed3ab4fd813f88647760b2bd3d894bc0aaef8f
KimYen <kimyen0816@gmail.com>
2014-07-16 16:20:33 -0700

    File Download Report  + add user data model - the return value of new metric

---
+++ src/main/java/org/sagebionetworks/dashboard/model/UserDataPoint.java
@@ -0,0 +1,30 @@
+package org.sagebionetworks.dashboard.model;
+
+public class UserDataPoint {
+
+    public UserDataPoint(String userId, String timestamp, String client) {
+        this.userId = userId;
+        this.timestamp = timestamp;
+        this.client = client;
+    }
+
+    public String userId() {
+        return this.userId;
+    }
+
+    public String timestamp() {
+        return this.timestamp;
+    }
+
+    public String client() {
+        return this.client;
+    }
+
+    public String toString() {
+        return this.timestamp + ":" + this.userId + ":" + this.client;
+    }
+
+    private String userId;
+    private String timestamp;
+    private String client;
+}
