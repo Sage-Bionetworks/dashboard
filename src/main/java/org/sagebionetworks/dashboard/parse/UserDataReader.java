b0aa159435cc7fcc2d01cfa73b27f6bf5b43683c
KimYen <kimyen0816@gmail.com>
2014-07-21 16:46:23 -0700

    use UserIdReader

--- src/main/java/org/sagebionetworks/dashboard/parse/UserDataReader.java
+++ src/main/java/org/sagebionetworks/dashboard/parse/UserDataReader.java
@@ -5,7 +5,7 @@ public class UserDataReader implements RecordReader<String> {
     @Override
     public String read(Record record) {
         Long timestamp = record.getTimestamp();
-        String userId = record.getUserId();
+        String userId = (new UserIdReader()).read(record);
         String client = (new ClientSummaryReader()).read(record);
         return Long.toString(timestamp) + ":" + userId + ":" + client;
     }
