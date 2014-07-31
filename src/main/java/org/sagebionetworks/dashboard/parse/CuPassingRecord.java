bd908b94eb529c91bbb07b6565d3c119c903e2d5
KimYen <kimyen0816@gmail.com>
2014-07-31 16:48:38 -0700

    Add a new class to represent the data get back from the rest api call: GET /user/{id}/certifiedUserPassingRecord

---
+++ src/main/java/org/sagebionetworks/dashboard/parse/CuPassingRecord.java
@@ -0,0 +1,53 @@
+package org.sagebionetworks.dashboard.parse;
+
+import java.util.ArrayList;
+
+/**
+ * This class represent a Certified User Quiz passing record.
+ * The fields in this class coresponse to the resulting fields in the REST API:
+ * GET /user/{id}/certifiedUserPassingRecord
+ */
+public class CuPassingRecord {
+
+    public CuPassingRecord(int responseId, boolean status, String userId,
+            String timestamp, int score, 
+            ArrayList<ResponseCorrectness>responseCorrectness, int quizId) {
+        this.responseId = responseId;
+        this.status = status;
+        this.userId = userId;
+        this.timestamp = timestamp;
+        this.score = score;
+        this.responseCorrectness = responseCorrectness;
+        this.quizId = quizId;
+    }
+
+    public int getResponseId() {
+        return responseId;
+    }
+    public boolean getStatus() {
+        return status;
+    }
+    public String getUserId() {
+        return userId;
+    }
+    public String getTimestamp() {
+        return timestamp;
+    }
+    public int getScore() {
+        return score;
+    }
+    public ArrayList<ResponseCorrectness> getReponseCorrectness() {
+        return responseCorrectness;
+    }
+    public int getQuizId() {
+        return quizId;
+    }
+
+    private int responseId;
+    private boolean status;
+    private String userId;
+    private String timestamp;
+    private int score;
+    private ArrayList<ResponseCorrectness> responseCorrectness;
+    private int quizId;
+}
