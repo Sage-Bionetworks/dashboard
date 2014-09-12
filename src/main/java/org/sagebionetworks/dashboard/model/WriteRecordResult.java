24d3cdcad9b3115110127525ccd4ce1891019f7c
Kimyen <kimyen0816@gmail.com>
2014-09-12 11:57:29 -0700

    add a class to store writing status

---
+++ src/main/java/org/sagebionetworks/dashboard/model/WriteRecordResult.java
@@ -0,0 +1,32 @@
+package org.sagebionetworks.dashboard.model;
+
+public class WriteRecordResult {
+
+    public WriteRecordResult(boolean status, String metric, String file, int line) {
+        this.status = status;
+        this.metric = metric;
+        this.file = file;
+        this.line = line;
+    }
+
+    public boolean status() {
+        return status;
+    }
+
+    public String metric() {
+        return metric;
+    }
+
+    public String file() {
+        return file;
+    }
+
+    public int line() {
+        return line;
+    }
+
+    private final boolean status;
+    private final String file;
+    private final String metric;
+    private final int line;
+}
