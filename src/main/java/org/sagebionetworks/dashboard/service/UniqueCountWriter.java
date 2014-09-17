aa1d5c3ce192ca905066be201be0f84ba8442ee3
Kimyen <kimyen0816@gmail.com>
2014-09-17 10:29:19 -0700

    remove duplicate code in UniqueCountWriter

--- src/main/java/org/sagebionetworks/dashboard/service/UniqueCountWriter.java
+++ src/main/java/org/sagebionetworks/dashboard/service/UniqueCountWriter.java
@@ -16,59 +16,6 @@ public class UniqueCountWriter<R extends Record> extends AbstractMetricWriter<R,
         uniqueCountDao.put(metricId, id, timestamp);
     }
 
-    /*public void writeCertifiedUsersMetric(CuPassingRecord record, CertifiedUserMetric metric) {
-        // Apply the filters first
-        List<RecordFilter<CuPassingRecord>> filters = metric.getFilters();
-        for (RecordFilter<CuPassingRecord> filter : filters) {
-            CertifiedUserFilter cuFilter = (CertifiedUserFilter) filter;
-            if (!cuFilter.matches(record)) {
-                return;
-            }
-        }
-
-        // Read the record
-        final String metricName = metric.getName();
-        final String metricId = nameIdDao.getId(metricName);
-        final DateTime timestamp = record.timestamp();
-        final CertifiedUserIdReader reader = (CertifiedUserIdReader) metric.getRecordReader();
-        final String value = reader.read(record);
-
-        // Write the metric
-        if (value != null) {
-            write(metricId, timestamp, value);
-        }
-    }*/
-
-    /*public void writeResponse(CuResponseRecord record, QuestionMetric metric, boolean passed) {
-        // Apply the filters first
-        List<RecordFilter<CuResponseRecord>> filters = metric.getFilters();
-        for (RecordFilter<CuResponseRecord> filter : filters) {
-            if (passed) {
-                QuestionPassFilter qpFilter = (QuestionPassFilter) filter;
-                if (!qpFilter.matches(record)) {
-                    return;
-                }
-            } else {
-                QuestionFailFilter qfFilter = (QuestionFailFilter) filter;
-                if (!qfFilter.matches(record)) {
-                    return;
-                }
-            }
-        }
-
-        // Read the record
-        final String metricName = metric.getName();
-        final String metricId = nameIdDao.getId(metricName);
-        final QuestionIndexReader reader = (QuestionIndexReader) metric.getRecordReader();
-        final String questionIndex = reader.read(record);
-        final String responseId = Integer.toString(record.responseId());
-
-        // Write the metric
-        if (questionIndex != null) {
-            write(metricId + ":" + questionIndex, record.timestamp(), responseId);
-        }
-    }*/
-
     public void removeValue(String userId, String metricName) {
         uniqueCountDao.removeValue(userId, nameIdDao.getId(metricName));
     }
