7a53f9b77a32751d53ff43aacd1ac5c440bea6ae
Kimyen <kimyen0816@gmail.com>
2014-09-17 15:21:57 -0700

    attempt to add getMetrics to Writer classes

--- src/main/java/org/sagebionetworks/dashboard/service/TimeSeriesWriter.java
+++ src/main/java/org/sagebionetworks/dashboard/service/TimeSeriesWriter.java
@@ -1,9 +1,12 @@
 package org.sagebionetworks.dashboard.service;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.joda.time.DateTime;
 import org.sagebionetworks.dashboard.dao.TimeSeriesDao;
+import org.sagebionetworks.dashboard.metric.Metric;
 import org.sagebionetworks.dashboard.parse.AccessRecord;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +20,12 @@ public class TimeSeriesWriter extends AbstractMetricWriter<AccessRecord, Long> {
     void write(String metricId, String additionalKey, DateTime timestamp,
             Long value) {
         timeSeriesDao.put(metricId + additionalKey, timestamp, value);
+    }
+
+    @Override
+    List<Metric<AccessRecord, Long>> getMetrics() {
+        // TODO Auto-generated method stub
+        return null;
     }
 
 }
--- src/test/java/org/sagebionetworks/dashboard/service/AbstractMetricWriterTest.java
+++ src/test/java/org/sagebionetworks/dashboard/service/AbstractMetricWriterTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.joda.time.DateTime;
@@ -25,6 +26,11 @@ public class AbstractMetricWriterTest {
         @Override
         void write(String metricId, String additionalKey, DateTime timestamp,
                 String id) {}
+
+        @Override
+        Collection<Metric<AccessRecord, String>> getMetrics() {
+            return null;
+        }
     }
 
     @Test
