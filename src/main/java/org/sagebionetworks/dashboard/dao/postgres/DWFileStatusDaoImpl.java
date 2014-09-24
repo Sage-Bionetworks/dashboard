647746abb089bb806e63382726dc554f2d307f98
Kimyen <kimyen0816@gmail.com>
2014-09-24 15:29:57 -0700

    add DWFileStatusDaoImpl

---
+++ src/main/java/org/sagebionetworks/dashboard/dao/postgres/DWFileStatusDaoImpl.java
@@ -0,0 +1,52 @@
+package org.sagebionetworks.dashboard.dao.postgres;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.sagebionetworks.dashboard.dao.FileStatusDao;
+import org.sagebionetworks.dashboard.model.FileFailure;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+public class DWFileStatusDaoImpl implements FileStatusDao{
+
+    @Resource
+    NamedParameterJdbcTemplate dwTemplate;
+
+    private final String query = "INSERT INTO file_status (file_path, status) VALUES (:file_path,:status);";
+
+    @Override
+    public boolean isCompleted(String file) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void setCompleted(String file) {
+        Map<String, Object> namedParameters = new HashMap<String, Object>();
+        namedParameters.put("file_path", file);
+        namedParameters.put("status", true);
+        dwTemplate.update(query, namedParameters);
+    }
+
+    @Override
+    public boolean isFailed(String file) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void setFailed(String file, int lineNumber) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public List<FileFailure> getFailures() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
--- src/main/resources/META-INF/spring/FileStatusTable.sql
+++ src/main/resources/META-INF/spring/FileStatusTable.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS file_status (
     file_path   varchar(100)    PRIMARY KEY,
-    line_number integer         NOT NULL,
     status      varchar(10)     NOT NULL
 );
 
