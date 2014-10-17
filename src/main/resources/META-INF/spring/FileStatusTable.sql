3d643de053076f611ee1abf1546bebdb789e99af
Kimyen <kimyen0816@gmail.com>
2014-10-17 16:44:29 -0700

    move postgres code to dw

--- src/main/resources/META-INF/spring/FileStatusTable.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS file_status (
-    file_path   varchar(100)    PRIMARY KEY,
-    status      varchar(10)     NOT NULL
-);
-
-CREATE INDEX ON file_status USING btree(status);
