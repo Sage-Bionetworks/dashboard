f4e01f5edfd1e7f31dbf7eae9a96fe807a832feb
Kimyen <kimyen0816@gmail.com>
2015-02-16 12:34:13 -0800

    add filters

--- src/main/java/org/sagebionetworks/dashboard/parse/UriTableFilter.java
+++ src/main/java/org/sagebionetworks/dashboard/parse/UriTableFilter.java
@@ -6,7 +6,7 @@ public class UriTableFilter extends UriRegexFilter {
 
     public UriTableFilter() {
         // /entity/{id}/table/...
-        super(Pattern.compile("^/repo/v1/entity/syn\\d+/table/$",
+        super(Pattern.compile("^/repo/v1/entity/syn\\d+/table/",
                 Pattern.CASE_INSENSITIVE));
     }
 
