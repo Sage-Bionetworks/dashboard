d6551a004ae5b4690bae26c5c2fdfbf4fde5479a
Kimyen <kimyen0816@gmail.com>
2015-02-16 19:17:36 -0800

    add TableUriMetric and TableClientMetric

---
+++ src/main/java/org/sagebionetworks/dashboard/metric/TableClientMetric.java
@@ -0,0 +1,44 @@
+package org.sagebionetworks.dashboard.metric;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.sagebionetworks.dashboard.parse.AccessRecord;
+import org.sagebionetworks.dashboard.parse.ClientSummaryReader;
+import org.sagebionetworks.dashboard.parse.ProdFilter;
+import org.sagebionetworks.dashboard.parse.RecordFilter;
+import org.sagebionetworks.dashboard.parse.RecordReader;
+import org.sagebionetworks.dashboard.parse.UriTableFilter;
+import org.sagebionetworks.dashboard.service.UniqueCountWriter;
+import org.springframework.stereotype.Component;
+
+@Component("tableClientMetric")
+public class TableClientMetric extends UniqueCountMetric<AccessRecord, String> {
+
+    private final List<RecordFilter<AccessRecord>> filters = 
+            Collections.unmodifiableList(Arrays.asList( new ProdFilter(), new UriTableFilter()));
+
+    private final RecordReader<AccessRecord, String> reader = new ClientSummaryReader();
+
+    @Resource
+    private UniqueCountWriter<AccessRecord> uniqueCountWriter;
+
+    @Override
+    public List<RecordFilter<AccessRecord>> getFilters() {
+        return filters;
+    }
+
+    @Override
+    public RecordReader<AccessRecord, String> getRecordReader() {
+        return reader;
+    }
+
+    @Override
+    public void write(AccessRecord record) {
+        uniqueCountWriter.writeMetric(record, this);
+    }
+
+}
